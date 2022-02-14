f11d585ae7c1f325e06a436fb739fb63cd296482
90306561 <72754329+90306561@users.noreply.github.com>
2022-02-14 10:49:21 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 90306561 on 2/14/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
